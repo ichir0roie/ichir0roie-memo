eabb4087e166dc41e1875ac11044f314c39975ad
ichir0roieNote <ichir0roie@gmail.com>
2021-11-10 13:12:27 +0900

    sample is moved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import ReactMarkdown from "react-markdown";
+// import ReactDom from "react-dom";
+import { Component } from "react";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: "# なんか書け！",
+			fileName: ".md",
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		// this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange = (event) => {
+		const { name, value } = event.target;
+
+		this.setState({
+			[name]: value,
+		});
+	};
+
+	render(h) {
+		// const text = "# なんか書け！";
+		const { text, fileName } = this.state;
+
+		return (
+			<div className="App">
+				<div id="mainBox">
+					<input
+						name="fileName"
+						id="inputFileName"
+						value={fileName}
+						onChange={this.handleChange}
+					></input>
+					<textarea
+						name="text"
+						id="inputMarkDown"
+						value={text}
+						onChange={this.handleChange}
+					></textarea>
+					<button>submit</button>
+				</div>
+				<div id="sideBox">
+					<div id="outPutMarkDown">
+						<ReactMarkdown>{text}</ReactMarkdown>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
